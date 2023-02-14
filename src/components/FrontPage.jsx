4024a85f7f3f5e01dcde68cc05acc8c21e4be254
Suyog Kalyan Nagawade <92072200+Suyog8383@users.noreply.github.com>
2023-02-14 14:32:19 +0530

    Update FrontPage.jsx

--- src/components/FrontPage.jsx
+++ src/components/FrontPage.jsx
@@ -28,7 +28,7 @@ function FrontPage() {
     <>
       <div style={box}>
         <img src="src\assets\github-logo.png" style={logoStyle} alt="" />
-        <h1>React GitHub Profiler</h1>
+        <h3>GitHub Profiler</h3>
         <div className="ui input focus">
           <input
             type="text"
